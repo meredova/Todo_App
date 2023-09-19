3999383b00e0fb3f02e6844fe7b5746d6235e322
Olga Meredova <olya.gurbo@gmail.com>
2023-09-19 20:40:45 +0300

    Add new project on React Native V1

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo'
 
 export default function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: title
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const removeTodo = (id) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title='Todo App!'/>
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo}/>
+
+        <FlatList
+          keyExtractor={item => item.id}
+          data = {todos}
+          renderItem={({item}) => (
+            <Todo todo={item} onRemove = {removeTodo}/>
+            )
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
